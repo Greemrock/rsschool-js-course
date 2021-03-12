cd00f9f5b570dae31ac1827d5a3abfdc2a96c20d
HOME-PC\Andrey <greemrock911@gmail.com>
2021-03-12 22:55:39 +0300

    feat: add toggle Notes/Letters

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -1,5 +1,6 @@
 const piano = document.querySelector('.piano');
-const pianoKey = document.querySelectorAll('.piano-key');
+const pianoKeys = document.querySelectorAll('.piano-key');
+
 function playAudio(src) {
     const audio = new Audio();
     audio.src = src;
@@ -7,17 +8,68 @@ function playAudio(src) {
     audio.play();
 }
 
-piano.addEventListener('mousedown', (e) => {
-    if(e.target.classList.contains('piano-key')) {
-		const note = e.target.dataset.note;
+function clickAudio(pianoKey) {
+	if(pianoKey.target.classList.contains('piano-key')) {
+		const note = pianoKey.target.dataset.note;
 		const src = `assets/audio/${note}.mp3`;
-		e.target.classList.add('piano-key-active','piano-key-active-pseudo');
-		e.target.classList.remove('piano-key-remove-mouse');
+		pianoKey.target.classList.add('piano-key-active','piano-key-active-pseudo');
+		pianoKey.target.classList.remove('piano-key-remove-mouse');
 		playAudio(src);
     }   
+}
+
+piano.addEventListener('mousedown', (event) => clickAudio(event));
+
+piano.addEventListener('mouseup', (event) => {
+	event.target.classList.add('piano-key-remove-mouse');
+	event.target.classList.remove('piano-key-active','piano-key-active-pseudo');
 });
 
-piano.addEventListener('mouseup', (e) => {
-	e.target.classList.add('piano-key-remove-mouse');
-	e.target.classList.remove('piano-key-active','piano-key-active-pseudo');
+window.addEventListener('keydown', (e) => {
+	pianoKeys.forEach(pianoKey => {
+		if (`Key${pianoKey.dataset.letter}` === e.code) {
+			const note = pianoKey.dataset.note;
+			const src = `assets/audio/${note}.mp3`;
+			pianoKey.classList.add('piano-key-active','piano-key-active-pseudo');
+			pianoKey.classList.remove('piano-key-remove-mouse');
+			playAudio(src);
+		}
+	});
 });
+
+window.addEventListener('keyup', (e) => {
+	pianoKeys.forEach(pianoKey => {
+		if (`Key${pianoKey.dataset.letter}` === e.code) {
+			pianoKey.classList.add('piano-key-remove-mouse');
+			pianoKey.classList.remove('piano-key-active','piano-key-active-pseudo');
+			
+		}
+	})
+});
+
+function toggleBtn() {
+	const btnContainer = document.querySelector('.btn-container');
+
+	btnContainer.addEventListener('click', (clickBtn) => {
+		const btns = document.querySelectorAll('.btn');
+		
+		if (clickBtn.target.classList.contains('btn-notes')) {
+			btnActive(btns, clickBtn);
+			pianoKeys.forEach(pianoKey => pianoKey.classList.remove('piano-key-letter'));
+		} else if (clickBtn.target.classList.contains('btn-letters')) {
+			btnActive(btns, clickBtn);
+			pianoKeys.forEach(pianoKey => pianoKey.classList.add('piano-key-letter'));
+		}
+	})
+}
+
+function btnActive(btns, clickBtn) {
+	btns.forEach(btn => {
+		if (btn.classList.contains('btn-active')) {
+			btn.classList.remove('btn-active');
+		}
+	})
+	clickBtn.target.classList.add('btn-active');
+}
+
+toggleBtn();
