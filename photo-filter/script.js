fdc47b6cc03cdd4cce901bc44966542a736ab95d
HOME-PC\Andrey <greemrock911@gmail.com>
2021-03-22 21:15:49 +0300

    feat: add func nextPicture

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -4,6 +4,10 @@ const OUTPUTS = document.querySelectorAll(".filters output");
 const IMG = document.querySelector(".editor img");
 const FILTERS = document.querySelector(".filters");
 const RESET = document.querySelector(".btn-reset");
+const NEXTPICTURE = document.querySelector(".btn-next");
+const BASE = 'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/night/';
+const IMAGES = ['01.jpg', '02.jpg', '03.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg'];
+let i = 0;
 
 function toggleFullScreen() {
 	if (!document.fullscreenElement) {
@@ -37,7 +41,24 @@ function reset() {
     }
 }
 
+function viewSrcImage(src) {  
+        const img = new Image();
+        img.src = src;
+        img.onload = () => {      
+            IMG.src = `${src}`;
+        }; 
+    }
+
+function nextPicture() {
+    const index = i % IMAGES.length;
+    const imageSrc = BASE + IMAGES[index];
+
+    viewSrcImage(imageSrc);
+    i++;
+}
+
 FULLSCREEN.addEventListener('click', toggleFullScreen);
 INPUTS.forEach(elem => elem.addEventListener('change', handleUpdate));
 INPUTS.forEach(elem => elem.addEventListener('mousemove', handleUpdate));
 RESET.addEventListener('click', reset);
+NEXTPICTURE.addEventListener('click', nextPicture);
