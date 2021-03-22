10df2fd81599ad1e797a58366956c410f7b1a533
HOME-PC\Andrey <greemrock911@gmail.com>
2021-03-22 18:45:54 +0300

    feat: add func outputUpdate

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -1,4 +1,8 @@
 const FULLSCREEN = document.querySelector(".openfullscreen");
+const INPUTS = document.querySelectorAll(".filters input");
+const OUTPUTS = document.querySelectorAll(".filters output");
+const IMG = document.querySelector(".editor img");
+const FILTERS = document.querySelector(".filters");
 
 function toggleFullScreen() {
 	if (!document.fullscreenElement) {
@@ -10,4 +14,20 @@ function toggleFullScreen() {
 	}
 }
 
+function handleUpdate() {
+    const suffix = this.dataset.sizing || '';
+    IMG.style.setProperty(`--${this.name}`, this.value + suffix);
+    outputUpdate(this);
+}
+
+function outputUpdate(elem) {
+    for (let i = 0; i < INPUTS.length; i++) {
+        if (elem.name == INPUTS.item(i).name) {
+            OUTPUTS.item(i).innerHTML = elem.value;
+        }
+    }
+}
+
 FULLSCREEN.addEventListener('click', toggleFullScreen);
+INPUTS.forEach(elem => elem.addEventListener('change', handleUpdate));
+INPUTS.forEach(elem => elem.addEventListener('mousemove', handleUpdate));
