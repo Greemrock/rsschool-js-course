db37d8ddeb006482d8e20e865ee96c7b0e1079d4
HOME-PC\Andrey <greemrock911@gmail.com>
2021-04-20 13:18:49 +0300

    feat: add burger-menu in live_brodcasts

--- online-zoo/pages/live_brodcast/live_brodcast.js
+++ online-zoo/pages/live_brodcast/live_brodcast.js
@@ -1,18 +1,26 @@
-var checkbox = document.querySelector('.theme__toggle');
+let checkbox = document.querySelector('.theme__toggle');
+let burger = document.querySelector('.header__burger');
+let navbar = document.querySelector('.navbar');
 
 checkbox.addEventListener('change', function() {
-    if(this.checked) {
-        trans()
-        document.documentElement.setAttribute('data-theme', 'dark')
-    } else {
-        trans()
-        document.documentElement.setAttribute('data-theme', 'light')
-    }
+  if(this.checked) {
+    trans()
+    document.documentElement.setAttribute('data-theme', 'dark')
+  } else {
+    trans()
+    document.documentElement.setAttribute('data-theme', 'light')
+  }
 })
 
 let trans = () => {
-    document.documentElement.classList.add('transition');
-    window.setTimeout(() => {
-        document.documentElement.classList.remove('transition')
-    }, 1000)
+  document.documentElement.classList.add('transition');
+  window.setTimeout(() => {
+    document.documentElement.classList.remove('transition')
+  }, 1000)
 }
+
+burger.addEventListener('click', (event) => {
+  burger.classList.toggle('active');
+  navbar.classList.toggle('active');
+  document.body.classList.toggle('lock');
+})
