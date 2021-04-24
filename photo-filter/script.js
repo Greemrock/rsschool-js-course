b5daf3b57cf44eca02d5cf6a6a7574c5bc9a4c95
HOME-PC\Andrey <greemrock911@gmail.com>
2021-04-24 16:01:23 +0300

    fix: add canvas in html and fix filter

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -1,19 +1,16 @@
 const FULLSCREEN = document.querySelector(".openfullscreen");
 const INPUTS = document.querySelectorAll(".filters input");
 const OUTPUTS = document.querySelectorAll(".filters output");
+const LABELS = document.querySelectorAll(".filters label");
 const IMG = document.querySelector(".editor img");
 const FILTERS = document.querySelector(".filters");
 const RESET = document.querySelector(".btn-reset");
 const NEXTPICTURE = document.querySelector(".btn-next");
 const FILEINPUT = document.querySelector('input[type="file"]');
 const FILEOUTPUT = document.querySelector('.btn-save');
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
 let i = 0;
-let blur = 0;
-let invert = 0;
-let sepia = 0;
-let saturate = 100;
-let hue = 0;
-let styleForCanvas = "";
 
 function toggleFullScreen() {
   if (!document.fullscreenElement) {
@@ -25,44 +22,47 @@ function toggleFullScreen() {
   }
 }
 
-function handleUpdate() {
-  IMG.style.filter = filter(this.name, this.value);
-  outputUpdate(this);
-}
-
-function filter(name, value) {
-  if (name == 'blur') blur = value;
-  if (name == 'invert') invert = value;
-  if (name == 'sepia') sepia = value;
-  if (name == 'saturate') saturate = value;
-  if (name == 'hue') hue = value;
-  styleForCanvas = `blur(${blur}px) invert(${invert}%) sepia(${sepia}%) saturate(${saturate}%) hue-rotate(${hue}deg)`;
-  return styleForCanvas;
-}
-
-function outputUpdate(elem) {
-  for (let i = 0; i < INPUTS.length; i++) {
-    if (elem.name == INPUTS.item(i).name) {
-      OUTPUTS.item(i).innerHTML = elem.value;
+function drawImage(src, config = "none") {
+  IMG.src = src;
+  IMG.setAttribute("crossOrigin", "anonymous");
+  IMG.onload = function () {
+    ratio = IMG.width / IMG.height;
+    if (IMG.height > 1200) {
+      canvas.style.width = `${100 * ratio}%`;
+      canvas.style.margin = "0 auto";
     }
-  }
+    canvas.width = IMG.width;
+    canvas.height = IMG.height;
+
+    ctx.filter = config;
+    ctx.drawImage(IMG, 0, 0);
+  };
+};
+
+function filter(elem) {
+    let config = [];
+    LABELS.forEach((i) => {
+      let input = i.querySelector("input");
+      let output = i.querySelector("output");
+
+      if (elem.target.classList.contains("btn-reset")) {
+        input.value = 0;
+        if (input.name === "saturate") {
+          input.value = 100;
+        }
+        ctx.filter = "blur(0px) invert(0%) sepia(0%) saturate(100%) hue-rotate(0deg)";
+        ctx.drawImage(IMG, 0, 0);
+      }
+      config.push(`${input.name}(${input.value}${input.dataset.sizing})`);
+      output.value = input.value;
+    });
+
+    drawImage(IMG.src, config.join(" "));
 }
 
-function reset() {
-  IMG.removeAttribute('style');
-  for (let i = 0; i < INPUTS.length; i++) {
-    INPUTS.item(i).value = INPUTS.item(i).defaultValue;
-    OUTPUTS.item(i).innerHTML = INPUTS.item(i).defaultValue;
-  }
-  styleForCanvas = ""
-}
-
-function viewSrcImage(src) {  
-  const img = new Image();
-  img.src = src;
-  img.onload = () => {      
-    IMG.src = `${src}`;
-  }; 
+if (i === 0) {
+  const loacalImg = "./assets/img/img.jpg";
+  drawImage(loacalImg);
 }
 
 function nextPicture() {
@@ -70,9 +70,11 @@ function nextPicture() {
   const IMAGES = ['01.jpg', '02.jpg', '03.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg'];
   const index = i % IMAGES.length;
   const imageSrc = `${BASE}/${pictureDependOnTheTime()}/${IMAGES[index]}`;
-
-  viewSrcImage(imageSrc);
+  canvas.style.width = "";
+  canvas.style = "";
+  drawImage(imageSrc, ctx.filter);
   i++;
+  
 }
 
 function pictureDependOnTheTime() {
@@ -97,30 +99,18 @@ function loadPicture() {
   const file = FILEINPUT.files[0];
   const reader = new FileReader();
   reader.onload = () => {
-    IMG.src = reader.result;
+    drawImage(reader.result, ctx.filter);
   }
   reader.readAsDataURL(file);
   FILEINPUT.value = "";
 }
 
 function savePicture() {
-  const canvas = document.createElement('canvas');
-  const ctx = canvas.getContext("2d");
-  const img = new Image();
-  img.setAttribute('crossOrigin', 'anonymous'); 
-  img.src = IMG.src;
-  img.onload = function() {
-    const index = ratio(img, canvas);
-    canvas.width = img.width;
-    canvas.height = img.height;
-    ctx.filter = `blur(${blur * index}px) invert(${invert}%) sepia(${sepia}%) saturate(${saturate}%) hue-rotate(${hue}deg)`;
-    ctx.drawImage(img, 0, 0);
-    let link = document.createElement('a');
-    link.download = 'download.png';
-    link.href = canvas.toDataURL('image/png');
-    link.click();
-    link.delete;
-  };
+  let link = document.createElement('a');
+  link.download = 'download.png';
+  link.href = canvas.toDataURL('image/png');
+  link.click();
+  link.delete;
 }
 
 function ratio(img, canvas) {
@@ -132,9 +122,8 @@ function ratio(img, canvas) {
 }
 
 FULLSCREEN.addEventListener('click', toggleFullScreen);
-INPUTS.forEach(elem => elem.addEventListener('change', handleUpdate));
-INPUTS.forEach(elem => elem.addEventListener('mousemove', handleUpdate));
-RESET.addEventListener('click', reset);
+FILTERS.addEventListener("input", filter);
+RESET.addEventListener('click', (e) => filter(e));
 NEXTPICTURE.addEventListener('click', nextPicture);
 FILEINPUT.addEventListener('change', loadPicture);
 FILEOUTPUT.addEventListener('click', savePicture);
