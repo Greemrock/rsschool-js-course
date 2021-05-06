7058fe352d1426d0a26d77e650455ba6be0ef5af
HOME-PC\Andrey <greemrock911@gmail.com>
2021-05-06 15:01:19 +0300

    feat: add block about

--- match-match-game/src/index.ts
+++ match-match-game/src/index.ts
@@ -1,3 +1,4 @@
+import About from './components/about';
 import Header from './components/header';
 import './styles.scss';
 
@@ -6,6 +7,7 @@ const app = window.document.getElementById('app');
 if (app) {
   app.innerHTML = `
     ${Header}
+    ${About}
   `;
 } else {
   console.error(`oops! app === ${app}`);
