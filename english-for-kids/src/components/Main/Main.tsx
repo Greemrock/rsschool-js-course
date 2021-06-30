e604a94a2d69dddc23014210f001788750932b25
HOME-PC\Andrey <greemrock911@gmail.com>
2021-06-30 15:15:28 +0300

    refactor: rename folder

--- english-for-kids/src/components/Main/Main.tsx
+++ english-for-kids/src/components/Main/Main.tsx
@@ -2,7 +2,7 @@ import { Redirect, Route, Switch } from "react-router-dom";
 import { IMainPageProps } from "../Shared/interface";
 import { CategoryCards } from "./CategoryCard/CategoryCards";
 import { CardsContainer, CardsList } from "./Styled/Game.styled";
-import { TrainCards } from "./TrainCard/TrainCards";
+import { TrainCards } from "./TrainMode/TrainCards";
 
 export const Main: React.FC<IMainPageProps> = ({ title, cards }) => {
   return (
